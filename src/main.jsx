dcd91cc56777249f707a115bf7809fb6f31c431b
TuanGauTruc12 <phamtuan12022001@gmail.com>
2023-10-11 09:00:18 +0700

    add @vercel/analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import App from "./App";
 import "./index.scss";
 import { store } from "./stores/store";
 import { Provider } from "react-redux";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
+    <Analytics />
   </Provider>
 );
